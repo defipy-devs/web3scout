2f25895a82ba8da9d1c76e16f7278880b56fe2dd
icmoore <ic3moore@gmail.com>
2024-09-12 09:27:30 -0700

    bug fix

--- python/prod/uniswap_v2/fetch_pair_details.py
+++ python/prod/uniswap_v2/fetch_pair_details.py
@@ -44,7 +44,7 @@ class FetchPairDetails:
             assert reverse_token_order is None, f"Give either (base_token_address, quote_token_address) or reverse_token_order"
             reverse_token_order = int(base_token_address, 16) > int(quote_token_address, 16)
     
-        pool = ABILoading().get_deployed_contract(web3, "sushi/UniswapV2Pair.json", pair_contact_address)
+        pool = ABILoad().get_deployed_contract(web3, "sushi/UniswapV2Pair.json", pair_contact_address)
         token0_address = pool.functions.token0().call()
         token1_address = pool.functions.token1().call()
     

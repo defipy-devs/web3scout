7ffdce6843a740c7ffa42de64abcdee8ac792dd9
icmoore <ic3moore@gmail.com>
2025-09-28 19:00:27 -0700

    v0.0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='Web3Scout',
-      version='0.0.6',
+      version='0.0.7',
       description='Onchain Event Framework for DeFiPy',
       long_description=long_description,
       long_description_content_type="text/markdown",
